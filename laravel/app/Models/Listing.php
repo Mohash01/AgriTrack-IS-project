5b60807ba971882efa9f39e382e754f5fad6a9bc
Mohash01 <muhammad.hamdun@strathmore.edu>
2024-06-22 14:57:11 +0300

    Kazi

--- laravel/app/Models/Listing.php
+++ laravel/app/Models/Listing.php
@@ -1,33 +1,11 @@
 <?php
-     namespace App\Models;
 
-     class Listing {
-        public static function all(){
-return [
-    [
-       'id' =>1,
-       'title' => 'Listing One',
-       'description' =>'Our web application centralizes digital record-keeping, providing real-time market insights and secure MPESA/bank payments. It automates administrative tasks, ensuring accurate reporting and enhanced operational efficiency. This solution improves data management, market access, and financial transparency for Kenvan dairy farmers.
-   '
-    ],
-   
-    [
-       'id' =>2,
-       'title' => 'Listing two',
-       'description' =>'Our web application centralizes digital record-keeping, providing real-time market insights and secure MPESA/bank payments. It automates administrative tasks, ensuring accurate reporting and enhanced operational efficiency. This solution improves data management, market access, and financial transparency for Kenvan dairy farmers.
-   '
-    ]
-    ];
-        }
-public static function find($id){
+namespace App\Models;
 
-    $listings = self::all();
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    foreach ($listings as $listing){
-        if ($listing['id']==$id){
-            return $listing;
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
-
-     }
